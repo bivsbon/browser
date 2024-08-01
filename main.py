d8b4b0abfa145ea136440e55e361698ce1fbb8ef
bivsbon <thanhminh20002005@gmail.com>
2024-08-01 12:01:11 +0700

    http, https, file and data scheme

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import ssl
 
 
 class URL:
+    _SUPPORTED_SCHEME = ["http", "https", "file", "data"]
+
     def __init__(self, url: str):
-        self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
+        self.scheme = self._get_scheme(url)
+        assert self.scheme
 
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
-
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
-
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+        if self.scheme == "http" or self.scheme == "https":
+            self._parse_url_http(url)
+        elif self. scheme == "file":
+            self._parse_url_file(url)
+        elif self.scheme == "data":
+            self._parse_url_data(url)
 
     def request(self) -> str:
+        if self.scheme == "http" or self.scheme == "https":
+            return self._request_http_and_https()
+        elif self.scheme == "file":
+            return self._request_file()
+        elif self.scheme == "data":
+            return self._request_data()
+
+    def _request_http_and_https(self) -> str:
         s = socket.socket(
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
@@ -34,6 +37,8 @@ class URL:
         s.connect((self.host, self.port))
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
+        request += "Connection: {}\r\n".format("close")
+        request += "User-Agent: {}\r\n".format("Quack Quack")
         request += "\r\n"
         s.send(request.encode("utf8"))
 
@@ -56,6 +61,45 @@ class URL:
         s.close()
 
         return content
+
+    def _request_file(self) -> str:
+        with open(self.url, "r") as f:
+            return f.read()
+
+    def _request_data(self) -> str:
+        return self.data
+
+    def _get_scheme(self, url: str):
+        for scheme in self._SUPPORTED_SCHEME:
+            if url.startswith(scheme):
+                return scheme
+        return None
+
+    def _parse_url_http(self, url: str):
+        url = url.split("://", 1)[1]
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+        self.url = url
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+    def _parse_url_file(self, url: str):
+        self.url = url.split("://", 1)[1]
+
+    def _parse_url_data(self, url: str):
+        data = url.split(":", 1)[1]
+        media, data = data.split(",", 1)
+        assert media in ["text/html"]
+        self.data = data
 
 
 def show(body):
